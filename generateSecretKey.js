2dfc13115f21d523e11f3885512c8ea77be167d9
Adi6360 <107831411+Adi6360@users.noreply.github.com>
2024-05-14 11:00:32 +0530

    Delete generateSecretKey.js

--- generateSecretKey.js
+++
@@ -1,9 +0,0 @@
-import crypto from 'crypto';
-
-// Generate a random buffer
-const secretKeyBuffer = crypto.randomBytes(32);
-
-// Convert buffer to a hex string
-const secretKey = secretKeyBuffer.toString('hex');
-
-console.log('Secure JWT Secret Key:', secretKey);
